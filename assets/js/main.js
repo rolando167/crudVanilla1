7a6d0ab0b4b5917d938af0fe9f5ef4bc64e4003e
rolando167 <rolando167@hotmail.com>
2021-05-22 16:26:14 -0500

    first

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,39 +9,39 @@
 
 	// ===== GET DATA ===== -->
 	// Method: GET
-	const renderData = () =>{
-		fetch(urlList)
-			.then(res => res.json())
-			.then(data => {
-				data.data.forEach((post, index) => {
 
-					let tableRow = document.createElement('tr');
-					tableRow.id = post.id;
-					// ------------
+	const renderData = (data) =>{
+		data.forEach((post, index) => {
+			let tableRow = document.createElement('tr');
+			tableRow.id = post.id;
+			// ------------
 
-					let listItem = `
-						<th scope="row">${post.id}</th>
-							<td>${post.nombre}</td>
-							<td>${post.email}</td>
-							<td>${post.monto}</td>
-							<td>${post.state}</td>
-							<td>${post.created_at}</td>
-							<td class="text-center">
-								<button type="button" class="btn btn-success mr-1"><i class='bx bxs-edit'></i></button>
-								<button type="button" class="btn btn-danger"><i class='bx bx-stop-circle'></i></button>
-							</td>
-					`;
-					tableRow.innerHTML = listItem ;
-					// ------------
-					console.log(post);
-					// ------------
+			let listItem = `
+					<th scope="row">${post.id}</th>
+					<td>${post.nombre}</td>
+					<td>${post.email}</td>
+					<td>${post.monto}</td>
+					<td>${post.state}</td>
+					<td>${post.created_at}</td>
+					<td class="text-center">
+					<button type="button" class="btn btn-outline-success mr-1"><i class='bx bxs-edit'></i></button>
+					<button type="button" class="btn btn-outline-danger"><i class='bx bx-stop-circle'></i></button>
+					</td>
+				`;
 
-					tableData.appendChild(tableRow);
-				});
-			});
+			tableRow.innerHTML = listItem ;
+			// ------------
+			// console.log(post);
+			// ------------
+			tableData.appendChild(tableRow);
+		});
 	}
-	
-	renderData();
+
+	 
+	fetch(urlList)
+		.then(res => res.json())
+		.then(data => renderData(data.data) );
+ 
 
 	// ===== GET DATA ===== -->
 	
