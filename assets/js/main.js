1a3b8a7368e167c191a7c0bc0297854e291cb73f
rolando167 <rolando167@hotmail.com>
2021-05-22 16:50:14 -0500

    first

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,12 +9,32 @@
 	// ===== GET DATA ===== -->
 	// Method: GET
 
-	const renderData = (data) => {
+	const renderData = (data) =>{
+		data.forEach((post, index) => {
+			let tableRow = document.createElement('tr');
+			tableRow.id = post.id;
+			// ------------
 
-		alert('ssss');
+			let listItem = `
+					<th scope="row">${post.id}</th>
+					<td>${post.nombre}</td>
+					<td>${post.email}</td>
+					<td>${post.monto}</td>
+					<td>${post.state}</td>
+					<td>${post.created_at}</td>
+					<td class="text-center">
+					<button type="button" class="btn btn-outline-success mr-1"><i class='bx bxs-edit'></i></button>
+					<button type="button" class="btn btn-outline-danger"><i class='bx bx-stop-circle'></i></button>
+					</td>
+				`;
+
+			tableRow.innerHTML = listItem ;
+			// ------------
+			// console.log(post);
+			// ------------
+			tableData.appendChild(tableRow);
+		});
 	}
-
-renderData('cas');
 
 	fetch(urlList)
 		.then(res => res.json())
